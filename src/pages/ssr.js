8ad696f1f83c09eb062c6f41b38e4059f612d715
mkarmark <mikarmar@microsoft.com>
2022-12-12 17:48:48 -0800

    Create ssr.js

---
+++ src/pages/ssr.js
@@ -0,0 +1,13 @@
+export default function SSR() {
+    return (
+      <>
+        <h1>Server-side rendered page</h1>
+        <p>
+          This page is server-side rendered. It was rendered yday.
+        </p>
+        <p>
+          <a href="/">View a static page.</a>
+        </p>
+      </>
+    );
+  }
