9696fb4b07710ffba5e79f061357949d6243ac18
mkarmark <mikarmar@microsoft.com>
2022-12-13 11:29:35 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <>
       <h1>Static page</h1>
-      <p>This page is static. It was built today.</p>
+      <p>This page is static. It was built 12/13/22 11:29am.</p>
       <p>
         <a href="/ssr">View a server-side rendered page.</a>
       </p>
